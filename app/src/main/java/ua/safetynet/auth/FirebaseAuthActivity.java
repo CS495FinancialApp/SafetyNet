b808cc02a2299e5a6869b780a08ae129a40d1bc0
Jeremy M <jjmccormick@crimson.ua.edu>
2018-09-14 13:51:22 -0500

    Got rid of currently unneeded imports

--- app/src/main/java/ua/safetynet/auth/FirebaseAuthActivity.java
+++ app/src/main/java/ua/safetynet/auth/FirebaseAuthActivity.java
@@ -2,22 +2,18 @@ package ua.safetynet.auth;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.Toast;
 
 import com.firebase.ui.auth.AuthUI;
 import com.firebase.ui.auth.IdpResponse;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
+
 
 import java.util.Arrays;
-import java.util.Collections;
+
 import java.util.List;
 
-import javax.security.auth.login.LoginException;
+
 
 import ua.safetynet.R;
 import ua.safetynet.user.MainPageActivity;
