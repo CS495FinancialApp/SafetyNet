0ed4a336d0fb9600655f222a70f475830f4b51a6
Jeremy M <jjmccormick@crimson.ua.edu>
2018-09-16 10:32:46 -0500

    Added logout button functionality. Somehow broke displaying user info.
    Signed-off-by: Jeremy M <jjmccormick@crimson.ua.edu>

--- app/src/main/java/ua/safetynet/user/MainPageActivity.java
+++ app/src/main/java/ua/safetynet/user/MainPageActivity.java
@@ -1,10 +1,17 @@
 package ua.safetynet.user;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
+import com.firebase.ui.auth.AuthUI;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
@@ -18,32 +25,67 @@ public class MainPageActivity extends AppCompatActivity
     private FirebaseUser firebaseUser;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState)
+    protected void onStart()
     {
-        super.onCreate(savedInstanceState);
-
+        super.onStart();
         firebaseAuth = FirebaseAuth.getInstance();
         firebaseUser = firebaseAuth.getCurrentUser();
 
-        if (firebaseUser == null)
+        if (firebaseUser == null) //If the user isnt already logged in
         {
             //User isnt signed in, launch sign in activity
             startActivity(new Intent(this, FirebaseAuthActivity.class));
         }
+    }
 
-            //Show main page, just show user info for temp
-            setContentView(R.layout.activity_main_page);
-            TextView username =  findViewById(R.id.username);
-            TextView useremail = findViewById(R.id.useremail);
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+
+        //Show main page, just show user info for temp
+        setContentView(R.layout.activity_main_page);
+        updateUI();
+        setLogoutListener(); //Set listener for logout button
+
+    }
+
+    /**
+     * Sets listener for logout button. Upon press signs user out using Firebase AuthUI
+     * and restarts main activity, thus prompting them to login again.
+     */
+    private void setLogoutListener()
+    {
+        setContentView(R.layout.activity_main_page);
+        Button logoutBtn = findViewById(R.id.logoutButton);
+        logoutBtn.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                    AuthUI.getInstance()
+                            .signOut(getApplicationContext())
+                            .addOnCompleteListener(new OnCompleteListener<Void>()
+                    {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task)
+                        {
+                            Intent intent = getIntent();
+                            finish();
+                            startActivity(intent);
+                        }
+                    });
+            }
+        });
+    }
+
+    private void updateUI()
+    {
+        TextView username =  findViewById(R.id.username);
+        TextView useremail = findViewById(R.id.useremail);
 
             username.setText(firebaseUser.getDisplayName());
             useremail.setText(firebaseUser.getEmail());
 
-
-    }
-
-    private void setToggleListener()
-    {
-        Button logoutBtn = findViewById(R.)
     }
 }
