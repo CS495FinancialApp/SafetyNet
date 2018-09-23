1909f1e591cce7069a7e9568e5cf10a3a6e40c6b
Jeremy M <jjmccormick@crimson.ua.edu>
2018-09-23 18:10:30 -0500

    Added some more to User class

--- app/src/main/java/ua/safetynet/user/User.java
+++ app/src/main/java/ua/safetynet/user/User.java
@@ -7,9 +7,59 @@ import java.util.ArrayList;
 
 public class User
 {
-    String userId;
-    String email;
+    private String userId;
+    private String email;
+    private String name;
 
-    ArrayList<String> transactions = new ArrayList<String>();
-    ArrayList<String> groups = new ArrayList<String>();
+    private ArrayList<String> transactions = new ArrayList<String>();
+    private ArrayList<String> groups = new ArrayList<String>();
+
+    public User()
+    {
+        userId = null;
+        email = null;
+        name = null;
+        transactions.clear();
+        groups.clear();
+    }
+
+    String getUserId()
+    {
+        return this.userId;
+    }
+
+    void setUserId(String userId)
+    {
+        this.userId = userId;
+    }
+
+    String getEmail()
+    {
+        return this.email;
+    }
+
+    void setEmail(String email)
+    {
+        this.email = email;
+    }
+
+    String getName()
+    {
+        return this.name;
+    }
+
+    void setName(String name)
+    {
+        this.name = name;
+    }
+
+    ArrayList<String> getTransactions()
+    {
+        return this.transactions;
+    }
+
+    ArrayList<String> getGroups()
+    {
+        return this.groups;
+    }
 }
