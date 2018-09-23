69772f68870d93c7333f184ea9212a487d5798be
Jeremy M <jjmccormick@crimson.ua.edu>
2018-09-23 11:54:23 -0500

    Thinking of things the user class will need

--- app/src/main/java/ua/safetynet/user/User.java
+++ app/src/main/java/ua/safetynet/user/User.java
@@ -3,7 +3,13 @@ package ua.safetynet.user;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
+import java.util.ArrayList;
+
 public class User
 {
+    String userId;
+    String email;
 
+    ArrayList<String> transactions = new ArrayList<String>();
+    ArrayList<String> groups = new ArrayList<String>();
 }
