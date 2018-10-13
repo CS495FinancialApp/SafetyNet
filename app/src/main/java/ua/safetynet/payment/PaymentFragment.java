93237f360d3d47ccaf022bac68583a2f9b2b2bf4
Jeremy <jjmccormick@crimson.ua.edu>
2018-10-13 16:54:12 -0500

    Filled out Payment Fragment. Still a WIP

--- app/src/main/java/ua/safetynet/payment/PaymentFragment.java
+++ app/src/main/java/ua/safetynet/payment/PaymentFragment.java
@@ -1,59 +1,58 @@
 package ua.safetynet.payment;
 
+import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import ua.safetynet.R;
+
+import com.braintreepayments.api.dropin.DropInActivity;
 import com.braintreepayments.api.dropin.DropInRequest;
+import com.braintreepayments.api.dropin.DropInResult;
+import com.braintreepayments.api.interfaces.PaymentMethodNonceCreatedListener;
+import com.braintreepayments.api.models.PaymentMethodNonce;
 import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.AsyncHttpResponseHandler;
+import com.loopj.android.http.JsonHttpResponseHandler;
 import com.loopj.android.http.RequestParams;
 import com.loopj.android.http.TextHttpResponseHandler;
+import java.math.BigDecimal;
 import cz.msebera.android.httpclient.Header;
 
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link PaymentFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link PaymentFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class PaymentFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
+public class PaymentFragment extends Fragment implements PaymentMethodNonceCreatedListener, View.OnClickListener {
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
+    private static final String TAG = "PAYMENT ACTIVITY";
+    private static final String AMOUNT = "amount";
+    private static final String GROUPID = "groupId";
+    private static int REQUEST_CODE;
+    private static final String APPENGINEURL = "http://safetynet-f2326.appspot.com/";
+    private static final String APPENGINETOKEN = "client_token";
+    private static final String APPENGINETRANS = "checkout";
+    private BigDecimal amount;
+    private String groupId = null;
+    private String userId = null;
+    private String clientToken = null;
     private OnFragmentInteractionListener mListener;
 
     public PaymentFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment PaymentFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static PaymentFragment newInstance(String param1, String param2) {
+    public static PaymentFragment newInstance(BigDecimal amount, String groupId) {
         PaymentFragment fragment = new PaymentFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
+        args.putDouble(AMOUNT, amount.doubleValue());
+        args.putString(GROUPID, groupId);
         fragment.setArguments(args);
         return fragment;
     }
@@ -62,8 +61,8 @@ public class PaymentFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+            amount = new BigDecimal(getArguments().getDouble(AMOUNT));
+            groupId = getArguments().getString(GROUPID);
         }
     }
 
@@ -74,13 +73,20 @@ public class PaymentFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_payment, container, false);
     }
 
-    // TODO: Rename method, update argument and hook method into UI event
+
     public void onButtonPressed(Uri uri) {
         if (mListener != null) {
             mListener.onFragmentInteraction(uri);
         }
     }
+    @Override
+    public void onClick(View v) {
+        EditText text = v.findViewById(R.id.payment_amount);
+        amount = new BigDecimal(Double.parseDouble(text.getText().toString()));
 
+        getClientToken();
+        launchDropIn();
+    }
     @Override
     public void onAttach(Context context) {
         super.onAttach(context);
@@ -98,6 +104,66 @@ public class PaymentFragment extends Fragment {
         mListener = null;
     }
 
+    private void launchDropIn() {
+        DropInRequest dropInRequest = new DropInRequest().clientToken(clientToken);
+        startActivityForResult(dropInRequest.getIntent(getContext()), REQUEST_CODE);
+    }
+
+    private void getClientToken(){
+        AsyncHttpClient client = new AsyncHttpClient(true,80,443);
+        RequestParams params = new RequestParams();
+        params.put("userId", userId);
+        client.get(APPENGINEURL + APPENGINETOKEN, params, new TextHttpResponseHandler() {
+            @Override
+            public void onFailure(int statusCode, Header[] headers, String responseString, Throwable throwable) {
+                Log.d(TAG,"Failed to fetch client token");
+            }
+
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, String responseString) {
+                clientToken = responseString;
+            }
+        });
+    }
+    private void createTransaction(PaymentMethodNonce nonce) {
+        AsyncHttpClient client = new AsyncHttpClient(true,80,443);
+        RequestParams params = new RequestParams();
+        params.put("payment_method_nonce", nonce);
+        params.put("amount", amount);
+        client.post(APPENGINEURL + APPENGINETRANS, params, new TextHttpResponseHandler() {
+            @Override
+            public void onFailure(int statusCode, Header[] headers, String responseString, Throwable throwable) {
+                Log.d(TAG,"Failed to send nonce to server");
+            }
+
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, String responseString) {
+                if(responseString != null && responseString.startsWith("created"))
+                    Toast.makeText(getContext(),"Transaction Complete", Toast.LENGTH_LONG).show();
+            }
+        });
+
+    }
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_CODE) {
+            if (resultCode == Activity.RESULT_OK) {
+                DropInResult result = data.getParcelableExtra(DropInResult.EXTRA_DROP_IN_RESULT);
+                createTransaction(result.getPaymentMethodNonce());
+            } else if (resultCode == Activity.RESULT_CANCELED) {
+                // the user canceled
+            } else {
+                // handle errors here, an exception may be available in
+                Exception error = (Exception) data.getSerializableExtra(DropInActivity.EXTRA_ERROR);
+            }
+        }
+    }
+
+    @Override
+    public void onPaymentMethodNonceCreated(PaymentMethodNonce paymentMethodNonce) {
+
+    }
     /**
      * This interface must be implemented by activities that contain this
      * fragment to allow an interaction in this fragment to be communicated
