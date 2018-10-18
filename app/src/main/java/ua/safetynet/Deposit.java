100d325b2295702d238a0c3f774fb3e1e0d8f56d
Jake Bailey <mjbailey2@crimson.ua.edu>
2018-10-17 21:42:34 -0500

    Created activities for withdrawal and deposit

---
+++ app/src/main/java/ua/safetynet/Deposit.java
@@ -0,0 +1,29 @@
+package ua.safetynet;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.EditText;
+
+public class Deposit extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_deposit);
+
+        final EditText dollarField = (EditText) findViewById(R.id.editDepositAmount);
+
+        dollarField.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void afterTextChanged(Editable s) {
+                dollarField.setText("$" + dollarField.getText().toString());
+            }
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {}
+        });
+    }
+}
