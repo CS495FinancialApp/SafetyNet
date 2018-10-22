447b29c2a352ad3d8933af68bb2a9753ccc905af
bdwilliams5 <bdwilliams5@crimson.ua.edu>
2018-10-21 21:09:22 -0500

    Finished the getGroup function and built a template for querying multiple users (can be easily converted for all future query needs such as pulling transfer and group data based on users or groups) to ideally make future queries easier.  Created a function (getUID) that returns the ID of the user that is currently logged into firebase.  Updated some notes in database class to be more clear and improved set functions for group and user based on Jeremy's example.  Removed redundant functions.

--- app/src/main/java/ua/safetynet/group/Group.java
+++ app/src/main/java/ua/safetynet/group/Group.java
@@ -11,6 +11,7 @@ import java.util.ArrayList;
 public class Group {
 
     private String Group_name;
+    private String Group_ID;
     private Integer Funds;
     private Integer Withdrawal_Limit;
 
@@ -20,6 +21,7 @@ public class Group {
 
     public Group() {
         this.Group_name = null;
+        this.Group_ID = null;
         this.Funds = 0;
         this.Withdrawal_Limit = 0;
 
@@ -28,8 +30,9 @@ public class Group {
         this.Users.clear();
     }
 
-    public Group(String group_name, Integer funds, Integer withdrawal_Limit, ArrayList<String> withdrawals, ArrayList<String> admins, ArrayList<String> users) {
+    public Group(String group_name,String group_id, Integer funds, Integer withdrawal_Limit, ArrayList<String> withdrawals, ArrayList<String> admins, ArrayList<String> users) {
         this.Group_name = group_name;
+        this.Group_ID = group_id;
         this.Funds = funds;
         this.Withdrawal_Limit = withdrawal_Limit;
         this.Withdrawals = withdrawals;
@@ -37,51 +40,59 @@ public class Group {
         this.Users = users;
     }
 
-    String getGroup_name() {
+    public String getGroup_name() {
         return this.Group_name;
     }
 
-    Integer getFunds() {
+    public String getGroup_ID() {
+        return Group_ID;
+    }
+
+    public Integer getFunds() {
         return this.Funds;
     }
 
-    Integer getWithdrawal_Limit() {
+    public Integer getWithdrawal_Limit() {
         return this.Withdrawal_Limit;
     }
 
-    ArrayList<String> getWithdrawals() {
+    public ArrayList<String> getWithdrawals() {
         return this.Withdrawals;
     }
 
-    ArrayList<String> getAdmins() {
+    public ArrayList<String> getAdmins() {
         return this.Admins;
     }
 
-    ArrayList<String> getUsers() {
+    public ArrayList<String> getUsers() {
         return this.Users;
     }
 
-    void setGroup_name(String group_name) {
+    public void setGroup_name(String group_name) {
         this.Group_name = group_name;
     }
 
-    void setFunds(Integer funds) {
+    public void setGroup_ID(String group_ID) {
+        this.Group_ID = group_ID;
+    }
+
+    public void setFunds(Integer funds) {
         this.Funds = funds;
     }
 
-    void setWithdrawal_Limit(Integer withdrawal_Limit) {
+    public void setWithdrawal_Limit(Integer withdrawal_Limit) {
         this.Withdrawal_Limit = withdrawal_Limit;
     }
 
-    void setWithdrawals(ArrayList<String> withdrawals) {
+    public void setWithdrawals(ArrayList<String> withdrawals) {
         this.Withdrawals = withdrawals;
     }
 
-    void setAdmins(ArrayList<String> admins) {
+    public void setAdmins(ArrayList<String> admins) {
         this.Admins = admins;
     }
 
-    void setUsers(ArrayList<String> users) {
+    public void setUsers(ArrayList<String> users) {
         this.Users = users;
     }
 }
