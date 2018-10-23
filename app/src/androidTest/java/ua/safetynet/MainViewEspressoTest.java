ddc5de0e2ab78469081d51b8991760f095627d83
Jeremy <jjmccormick@crimson.ua.edu>
2018-10-23 08:27:50 -0500

    Test: Added some more stuff

--- app/src/androidTest/java/ua/safetynet/MainViewEspressoTest.java
+++ app/src/androidTest/java/ua/safetynet/MainViewEspressoTest.java
@@ -1,4 +1,17 @@
 package ua.safetynet;
 
+import android.support.test.filters.LargeTest;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(AndroidJUnit4.class)
+@LargeTest
 public class MainViewEspressoTest {
+
+    @Test
+    public void loginViewShown() {
+        onView
+    }
 }
