8b730e851757ac2d63724b03185755776f36de26
Jeremy <jjmccormick@crimson.ua.edu>
2018-11-05 10:59:18 -0600

    Group: Added toMap and fromMap for group obj

--- app/src/main/java/ua/safetynet/group/Group.java
+++ app/src/main/java/ua/safetynet/group/Group.java
@@ -22,14 +22,16 @@ import java.io.File;
 import java.lang.reflect.Array;
 import java.math.BigDecimal;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Group {
 
     private String Group_name;
     private String Group_ID;
     private Bitmap groupImage;
-    private double Funds;
-    private double Withdrawal_Limit;
+    private BigDecimal Funds;
+    private BigDecimal Withdrawal_Limit;
 
     private ArrayList<String> Withdrawals = new ArrayList<>();
     private ArrayList<String> Admins = new ArrayList<>();
@@ -38,17 +40,17 @@ public class Group {
     public Group() {
         this.Group_name = null;
         this.Group_ID = null;
-        this.Funds = 0;
-        this.Withdrawal_Limit = 50;
+        this.Funds = new BigDecimal(0);
+        this.Withdrawal_Limit = new BigDecimal(50);
 
         this.Withdrawals.clear();
         this.Admins.clear();
         this.Users.clear();
 
-        fetchGroupImage();
+        //fetchGroupImage();
     }
 
-    public Group(String group_name,String group_id, double funds, double withdrawal_Limit, ArrayList<String> withdrawals, ArrayList<String> admins, ArrayList<String> users) {
+    public Group(String group_name,String group_id, BigDecimal funds, BigDecimal withdrawal_Limit, ArrayList<String> withdrawals, ArrayList<String> admins, ArrayList<String> users) {
         this.Group_name = group_name;
         this.Group_ID = group_id;
         this.Funds = funds;
@@ -60,7 +62,7 @@ public class Group {
         fetchGroupImage();
     }
 
-    public Group(String group_name,String group_id, Bitmap groupImage, double funds, double withdrawal_Limit, ArrayList<String> withdrawals, ArrayList<String> admins, ArrayList<String> users) {
+    public Group(String group_name,String group_id, Bitmap groupImage, BigDecimal funds, BigDecimal withdrawal_Limit, ArrayList<String> withdrawals, ArrayList<String> admins, ArrayList<String> users) {
         this.Group_name = group_name;
         this.Group_ID = group_id;
         this.Funds = funds;
@@ -79,11 +81,11 @@ public class Group {
         return Group_ID;
     }
 
-    public double getFunds() {
+    public BigDecimal getFunds() {
         return this.Funds;
     }
 
-    public double getWithdrawal_Limit() {
+    public BigDecimal getWithdrawal_Limit() {
         return this.Withdrawal_Limit;
     }
 
@@ -107,11 +109,11 @@ public class Group {
         this.Group_ID = group_ID;
     }
 
-    public void setFunds(double funds) {
+    public void setFunds(BigDecimal funds) {
         this.Funds = funds;
     }
 
-    public void setWithdrawal_Limit(double withdrawal_Limit) {
+    public void setWithdrawal_Limit(BigDecimal withdrawal_Limit) {
         this.Withdrawal_Limit = withdrawal_Limit;
     }
 
@@ -178,6 +180,78 @@ public class Group {
     }
 
     public Bitmap getGroupImage() {
+        if(this.groupImage == null)
+            fetchGroupImage();
         return groupImage;
     }
+
+    public Map<String, Object> toMap() {
+        Map<String, Object> map = new HashMap<>();
+        map.put("groupId",this.Group_ID);
+        map.put("name", this.Group_name);
+        map.put("funds", this.Funds.toString());
+        map.put("limit", this.Withdrawal_Limit.toString());
+        Map<String, String> transMap = new HashMap<>();
+        for(Integer i =0; i < this.Withdrawals.size(); i++ )
+        {
+            transMap.put("transaction" + i.toString(), this.Withdrawals.get(i));
+        }
+        map.put("transactions",transMap);
+
+        Map<String, String> memberMap = new HashMap<>();
+        for(Integer i =0; i < this.Users.size(); i++ )
+        {
+            memberMap.put("member" + i.toString(), this.Users.get(i));
+        }
+        map.put("members",memberMap);
+
+        Map<String, String> adminMap = new HashMap<>();
+        for(Integer i =0; i < this.Admins.size(); i++ )
+        {
+            memberMap.put("admin" + i.toString(), this.Admins.get(i));
+        }
+        map.put("admins",adminMap);
+
+        return map;
+    }
+
+    public static Group fromMap(Map<String, Object> map) {
+        Group group = new Group();
+        group.setGroup_ID(map.get("groupId").toString());
+        group.setGroup_name(map.get("name").toString());
+        group.setFunds(new BigDecimal(map.get("funds").toString()));
+        group.setWithdrawal_Limit(new BigDecimal(map.get("limit").toString()));
+
+        Map<String, String> transMap = (Map<String, String>)map.get("transactions");
+        ArrayList<String> trans = new ArrayList<>();
+        for(Integer i = 0; ; i++) {
+            if(transMap.containsKey("transaction" + i.toString()))
+                trans.add(transMap.get("transaction" + i.toString()));
+            else
+                break;
+        }
+        group.setWithdrawals(trans);
+
+        Map<String, String> memberMap = (Map<String, String>)map.get("members");
+        ArrayList<String> members = new ArrayList<>();
+        for(Integer i = 0; ; i++) {
+            if(memberMap.containsKey("member" + i.toString()))
+                members.add(memberMap.get("member" + i.toString()));
+            else
+                break;
+        }
+        group.setUsers(members);
+
+        Map<String, String> adminMap = (Map<String, String>)map.get("admins");
+        ArrayList<String> admins = new ArrayList<>();
+        for(Integer i = 0; ; i++) {
+            if(adminMap.containsKey("admin" + i.toString()))
+                admins.add(adminMap.get("admin" + i.toString()));
+            else
+                break;
+        }
+        group.setAdmins(admins);
+        return group;
+    }
+
 }
