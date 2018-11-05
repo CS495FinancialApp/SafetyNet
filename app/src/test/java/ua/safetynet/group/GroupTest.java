8f031fe9fb16039a8b3a33a8e0ffbecdc542d0ad
Jeremy <jjmccormick@crimson.ua.edu>
2018-11-05 10:59:48 -0600

    Testing: Added Junit test for to/fromMap in Group

---
+++ app/src/test/java/ua/safetynet/group/GroupTest.java
@@ -0,0 +1,52 @@
+package ua.safetynet.group;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Map;
+
+@RunWith(JUnit4.class)
+public class GroupTest {
+    Group group;
+    @Before
+    public void makeUser() {
+        group = new Group();
+        group.setGroup_ID("123456789");
+        group.setGroup_name("John Doe");
+        group.setWithdrawal_Limit(new BigDecimal(20));
+        group.setFunds(new BigDecimal(150));
+        ArrayList<String> trans = new ArrayList<String>();
+        trans.add("1");
+        trans.add("2");
+        trans.add("3");
+        group.setWithdrawals(trans);
+        ArrayList<String> users = new ArrayList<String>();
+        users.add("4");
+        users.add("5");
+        users.add("6");
+        group.setUsers(users);
+        ArrayList<String> admins = new ArrayList<String>();
+        admins.add("7");
+        admins.add("8");
+        admins.add("9");
+        group.setUsers(admins);
+    }
+    @Test
+    public void testToFromMap() {
+        Map<String, Object> map = group.toMap();
+        Group mapGroup= Group.fromMap(map);
+        Assert.assertEquals(group.getGroup_ID(), mapGroup.getGroup_ID());
+        Assert.assertEquals(group.getGroup_name(), mapGroup.getGroup_name());
+        Assert.assertEquals(group.getFunds(), mapGroup.getFunds());
+        Assert.assertEquals(group.getWithdrawal_Limit(), mapGroup.getWithdrawal_Limit());
+        Assert.assertEquals(group.getWithdrawals(), mapGroup.getWithdrawals());
+        Assert.assertEquals(group.getUsers(), mapGroup.getUsers());
+        Assert.assertEquals(group.getAdmins(), mapGroup.getAdmins());
+    }
+}
+
+
