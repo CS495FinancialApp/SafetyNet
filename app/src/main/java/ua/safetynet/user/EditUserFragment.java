1c46c4090ad9f55d7443bb8534bc684606c7c740
Jeremy <jjmccormick@crimson.ua.edu>
2018-11-18 16:15:00 -0600

    EditUser: View now properly populating

--- app/src/main/java/ua/safetynet/user/EditUserFragment.java
+++ app/src/main/java/ua/safetynet/user/EditUserFragment.java
@@ -13,6 +13,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
 
+import com.bumptech.glide.Glide;
 import com.firebase.ui.auth.AuthUI;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.auth.FirebaseAuth;
@@ -24,13 +25,12 @@ import ua.safetynet.R;
 
 public class EditUserFragment extends Fragment {
 
-    private User user;
+    private User user = null;
     private OnFragmentInteractionListener mListener;
     private TextInputEditText nameText;
     private TextInputEditText emailText;
     private TextInputEditText phoneText;
     private CircleImageView imageView;
-
     public EditUserFragment() {
         // Required empty public constructor
     }
@@ -57,14 +57,17 @@ public class EditUserFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_edit_user, container, false);
-        nameText = view.findViewById(R.id.edit_user_name);
-        emailText = view.findViewById(R.id.edit_user_email);
-        phoneText = view.findViewById(R.id.edit_phone_number);
-        setupPhoneInput();
-        populateText();
         return view;
     }
-
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        nameText = view.findViewById(R.id.edit_user_name);
+        emailText = view.findViewById(R.id.edit_user_email);
+        phoneText = view.findViewById(R.id.edit_user_phone);
+        imageView = view.findViewById(R.id.edit_user_image);
+        populateText();
+        setupPhoneInput();
+    }
     // TODO: Rename method, update argument and hook method into UI event
     public void onButtonPressed(Uri uri) {
         if (mListener != null) {
@@ -90,78 +93,26 @@ public class EditUserFragment extends Fragment {
     }
 
     private void populateText() {
-        FirebaseUser firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
-        if(firebaseUser.getDisplayName() != null)
-            nameText.setText(firebaseUser.getDisplayName());
-        if(firebaseUser.getEmail() != null)
-            emailText.setText(firebaseUser.getEmail());
-        if(!firebaseUser.getPhoneNumber().isEmpty())
-            phoneText.setText(firebaseUser.getPhoneNumber());
-        if(firebaseUser.getPhotoUrl().toString().isEmpty())
-            imageView.setImageURI(firebaseUser.getPhotoUrl());
+        if(user == null) {
+            FirebaseUser firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+            if (firebaseUser.getDisplayName() != null)
+                nameText.setText(firebaseUser.getDisplayName());
+            if (firebaseUser.getEmail() != null)
+                emailText.setText(firebaseUser.getEmail());
+            if (!firebaseUser.getPhoneNumber().isEmpty())
+                phoneText.setText(firebaseUser.getPhoneNumber());
+            if (firebaseUser.getPhotoUrl() != null) {
+                Glide.with(getContext()).load(firebaseUser.getPhotoUrl()).into(imageView);
+            }
+        }
+
     }
 
     private void setupPhoneInput() {
-        phoneText.addTextChangedListener(new TextWatcher() {
-            //we need to know if the user is erasing or inputing some new character
-            private boolean backspacingFlag = false;
-            //we need to block the :afterTextChanges method to be called again after we just replaced the EditText text
-            private boolean editedFlag = false;
-            //we need to mark the cursor position and restore it after the edition
-            private int cursorComplement;
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-                //we store the cursor local relative to the end of the string in the EditText before the edition
-                cursorComplement = s.length()-phoneText.getSelectionStart();
-                //we check if the user ir inputing or erasing a character
-                if (count > after) {
-                    backspacingFlag = true;
-                } else {
-                    backspacingFlag = false;
-                }
-            }
-            @Override
-            public void onTextChanged(CharSequence s, int start, int count, int after) {
-
-            }
-            @Override
-            public void afterTextChanged(Editable s) {
-                String string = s.toString();
-                //what matters are the phone digits beneath the mask, so we always work with a raw string with only digits
-                String phone = string.replaceAll("[^\\d]", "");
-
-                //if the text was just edited, :afterTextChanged is called another time... so we need to verify the flag of edition
-                //if the flag is false, this is a original user-typed entry. so we go on and do some magic
-                if (!editedFlag) {
-
-                    //we start verifying the worst case, many characters mask need to be added
-                    //example: 999999999 <- 6+ digits already typed
-                    // masked: (999) 999-999
-                    if (phone.length() >= 6 && !backspacingFlag) {
-                        //we will edit. next call on this textWatcher will be ignored
-                        editedFlag = true;
-                        //here is the core. we substring the raw digits and add the mask as convenient
-                        String ans = "(" + phone.substring(0, 3) + ") " + phone.substring(3,6) + "-" + phone.substring(6);
-                        phoneText.setText(ans);
-                        //we deliver the cursor to its original position relative to the end of the string
-                        phoneText.setSelection(phoneText.getText().length()-cursorComplement);
-
-                        //we end at the most simple case, when just one character mask is needed
-                        //example: 99999 <- 3+ digits already typed
-                        // masked: (999) 99
-                    } else if (phone.length() >= 3 && !backspacingFlag) {
-                        editedFlag = true;
-                        String ans = "(" +phone.substring(0, 3) + ") " + phone.substring(3);
-                        phoneText.setText(ans);
-                        phoneText.setSelection(phoneText.getText().length()-cursorComplement);
-                    }
-                    // We just edited the field, ignoring this cicle of the watcher and getting ready for the next
-                } else {
-                    editedFlag = false;
-                }
-            }
-        });
+        if(phoneText != null)
+            phoneText.addTextChangedListener(new PhoneNumberFormattingTextWatcher("US"));
     }
+
     public interface OnFragmentInteractionListener {
         // TODO: Update argument type and name
         void onFragmentInteraction(Uri uri);
