63f1a24569812ea12c7a20a5bb18aaf7ffbf790d
Jeremy <jjmccormick@crimson.ua.edu>
2018-11-18 23:48:53 -0600

    EditUser: Add done button and update user obj

--- app/src/main/java/ua/safetynet/user/EditUserFragment.java
+++ app/src/main/java/ua/safetynet/user/EditUserFragment.java
@@ -1,6 +1,9 @@
 package ua.safetynet.user;
 
 import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.TextInputEditText;
@@ -8,10 +11,15 @@ import android.support.v4.app.Fragment;
 import android.telephony.PhoneNumberFormattingTextWatcher;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 import com.firebase.ui.auth.AuthUI;
@@ -19,7 +27,10 @@ import com.google.firebase.FirebaseApp;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
+import java.io.InputStream;
+
 import de.hdodenhof.circleimageview.CircleImageView;
+import ua.safetynet.Database;
 import ua.safetynet.R;
 
 
@@ -31,6 +42,10 @@ public class EditUserFragment extends Fragment {
     private TextInputEditText emailText;
     private TextInputEditText phoneText;
     private CircleImageView imageView;
+    private Bitmap image;
+    public static final String TAG = "Edit User Fragment";
+    public static final int PICK_IMAGE = 1;
+
     public EditUserFragment() {
         // Required empty public constructor
     }
@@ -39,7 +54,7 @@ public class EditUserFragment extends Fragment {
     public static EditUserFragment newInstance(User user) {
         EditUserFragment fragment = new EditUserFragment();
         Bundle args = new Bundle();
-        args.putSerializable("user", user);
+        //args.putStr("user", user);
         fragment.setArguments(args);
         return fragment;
     }
@@ -50,6 +65,7 @@ public class EditUserFragment extends Fragment {
         if (getArguments() != null) {
             user = (User)savedInstanceState.getSerializable("user");
         }
+        setHasOptionsMenu(true);
     }
 
     @Override
@@ -65,8 +81,29 @@ public class EditUserFragment extends Fragment {
         emailText = view.findViewById(R.id.edit_user_email);
         phoneText = view.findViewById(R.id.edit_user_phone);
         imageView = view.findViewById(R.id.edit_user_image);
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                selectImage();
+            }
+        });
         populateText();
         setupPhoneInput();
+    }
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.done_check_toolbar, menu);
+        super.onCreateOptionsMenu(menu,inflater);
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        switch (id) {
+            case R.id.edit_user_done:
+                storeUser();
+                return true;
+        }
+        return super.onOptionsItemSelected(item); // important line
     }
     // TODO: Rename method, update argument and hook method into UI event
     public void onButtonPressed(Uri uri) {
@@ -93,16 +130,30 @@ public class EditUserFragment extends Fragment {
     }
 
     private void populateText() {
+
         if(user == null) {
             FirebaseUser firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
-            if (firebaseUser.getDisplayName() != null)
-                nameText.setText(firebaseUser.getDisplayName());
-            if (firebaseUser.getEmail() != null)
-                emailText.setText(firebaseUser.getEmail());
-            if (!firebaseUser.getPhoneNumber().isEmpty())
-                phoneText.setText(firebaseUser.getPhoneNumber());
-            if (firebaseUser.getPhotoUrl() != null) {
-                Glide.with(getContext()).load(firebaseUser.getPhotoUrl()).into(imageView);
+            if(firebaseUser != null) {
+                if (firebaseUser.getDisplayName() != null)
+                    nameText.setText(firebaseUser.getDisplayName());
+                if (firebaseUser.getEmail() != null)
+                    emailText.setText(firebaseUser.getEmail());
+                if (!firebaseUser.getPhoneNumber().isEmpty())
+                    phoneText.setText(firebaseUser.getPhoneNumber());
+                if (firebaseUser.getPhotoUrl() != null) {
+                    Glide.with(getContext()).load(firebaseUser.getPhotoUrl()).into(imageView);
+                }
+            }
+        }
+        else {
+            if (user.getName() != null)
+                nameText.setText(user.getName());
+            if (user.getEmail() != null)
+                emailText.setText(user.getEmail());
+            if (user.getPhoneNumber() != null)
+                phoneText.setText(user.getPhoneNumber());
+            if (user.getImage() != null) {
+                Glide.with(getContext()).load(user.getImage()).into(imageView);
             }
         }
 
@@ -112,9 +163,56 @@ public class EditUserFragment extends Fragment {
         if(phoneText != null)
             phoneText.addTextChangedListener(new PhoneNumberFormattingTextWatcher("US"));
     }
+    private void selectImage() {
+        Intent getIntent = new Intent(Intent.ACTION_GET_CONTENT);
+        getIntent.setType("image/*");
 
+        Intent pickIntent = new Intent(Intent.ACTION_PICK);
+        pickIntent.setDataAndType(android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI,"image/*");
+
+        Intent chooserIntent = Intent.createChooser(getIntent, "Select Image");
+        chooserIntent.putExtra(Intent.EXTRA_INITIAL_INTENTS, new Intent[] {pickIntent});
+
+        startActivityForResult(chooserIntent, PICK_IMAGE);
+    }
+    private void storeUser() {
+        if(user==null) {
+            user = new User();
+            user.setUserId(FirebaseAuth.getInstance().getUid());
+            user.setName(nameText.getText().toString());
+            user.setEmail(emailText.getText().toString());
+            user.setPhoneNumber(phoneText.getText().toString());
+            user.setImage(((BitmapDrawable) imageView.getDrawable()).getBitmap());
+        }
+        else {
+            user.setName(nameText.getText().toString());
+            user.setEmail(emailText.getText().toString());
+            user.setPhoneNumber(phoneText.getText().toString());
+            user.setImage(((BitmapDrawable) imageView.getDrawable()).getBitmap());
+        }
+
+            Database db = new Database();
+            db.setUser(user);
+    }
     public interface OnFragmentInteractionListener {
         // TODO: Update argument type and name
         void onFragmentInteraction(Uri uri);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == PICK_IMAGE) {
+            if(data == null) {
+                Log.d(TAG, "Couldn't get image from storage");
+                Toast.makeText(getContext(), "Could not get photo from storage",Toast.LENGTH_SHORT ).show();
+                return;
+            }
+            final Bundle extras = data.getExtras();
+            if(extras != null) {
+                Bitmap newImage = extras.getParcelable("data");
+                imageView.setImageBitmap(newImage);
+            }
+        }
     }
 }
