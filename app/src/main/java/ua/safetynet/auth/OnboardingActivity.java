354d8980c7d06d8bd4ea78c9fe544371b601b335
Jeremy <jjmccormick@crimson.ua.edu>
2018-11-30 23:45:31 -0600

    Onboard: Remove unndeed lines from activity

--- app/src/main/java/ua/safetynet/auth/OnboardingActivity.java
+++ app/src/main/java/ua/safetynet/auth/OnboardingActivity.java
@@ -108,8 +108,6 @@ public class OnboardingActivity extends AppCompatActivity {
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
             View rootView = inflater.inflate(R.layout.fragment_onboarding, container, false);
-            TextView textView = (TextView) rootView.findViewById(R.id.onboarding_section_label);
-            textView.setText(getString(R.string.section_format, getArguments().getInt(ARG_SECTION_NUMBER)));
             return rootView;
         }
     }
