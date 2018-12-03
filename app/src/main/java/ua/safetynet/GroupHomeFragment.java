f9d7fbc2fceeb748c87205ec013eaf5fb2c346f5
Jeremy McCormick <jjmccormick@crimson.ua.edu>
2018-12-02 23:22:21 -0600

    GroupHome: Get admins from group obj and set onclick

--- app/src/main/java/ua/safetynet/GroupHomeFragment.java
+++ app/src/main/java/ua/safetynet/GroupHomeFragment.java
@@ -3,6 +3,7 @@ package ua.safetynet;
 import android.content.Intent;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -16,6 +17,7 @@ import com.google.firebase.auth.FirebaseAuth;
 import java.text.NumberFormat;
 import java.util.Locale;
 
+import ua.safetynet.group.EditGroupFragment;
 import ua.safetynet.group.Group;
 import ua.safetynet.user.User;
 import ua.safetynet.user.ViewUserFragment;
@@ -28,6 +30,7 @@ public class GroupHomeFragment extends Fragment {
     private static final String GROUP = "group";
     private static final String TAG = "GROUP HOME FRAGMENT";
     private Group group;
+
 
     public GroupHomeFragment() {
         // Required empty public constructor
@@ -61,27 +64,15 @@ public class GroupHomeFragment extends Fragment {
         final TextView name = view.findViewById(R.id.textViewGroupName);
         final TextView balance = view.findViewById(R.id.textViewGroupBalance);
         final NumberFormat format = NumberFormat.getCurrencyInstance(Locale.US);    //Dollar format
-        //final Intent UserIntent = new Intent(GroupHomeFragment.this, UserTransactionHistory.class);
-        //final Intent EditIntent = new Intent(GroupHomeFragment.this, EditGroup.class);
-        //final Intent GroupIntent = new Intent(GroupHomeFragment.this, GroupTransactionHistory.class);
+        //final Intent UserIntent = new Intent(EditGroupFragment.this, UserTransactionHistory.class);
+        //final Intent EditIntent = new Intent(EditGroupFragment.this, EditGroupFragment.class);
+        //final Intent GroupIntent = new Intent(EditGroupFragment.this, GroupTransactionHistory.class);
         Database db = new Database();
 
-        final boolean[] adminFlag = {false};
 
-        db.getGroup(group.getGroupId(), new Database.DatabaseGroupListener() {
-            @Override
-            public void onGroupRetrieval(Group group) {
-                name.setText(group.getName());
-                balance.setText("Balance: " + format.format(group.getFunds()));
-                //Pass groupID for future retrieval
-                //GroupIntent.putExtra("group_ID", group.getGroupId());
-                //EditIntent.putExtra("group_ID", group.getGroupId());
-                //UserIntent.putExtra("group_ID", group.getGroupId());
-                if (group.getAdmins().contains(FirebaseAuth.getInstance().getCurrentUser().getUid())){
-                    adminFlag[0] = true;
-                }
-            }
-        });
+        boolean isAdmin = (FirebaseAuth.getInstance().getCurrentUser() != null && group.getAdmins().contains(FirebaseAuth.getInstance().getCurrentUser().getUid()));
+        name.setText(group.getName());
+        balance.setText("Balance: " + format.format(group.getFunds()));
 
         //Goes to UserTransactionHistory Activity
         userHistory.setOnClickListener(new View.OnClickListener() {
@@ -90,17 +81,16 @@ public class GroupHomeFragment extends Fragment {
             }
         });
 
-        editGroup.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (adminFlag[0]) {
-                    //startActivity(EditIntent);
-                }
-                else {
-                    Toast.makeText(getContext(), "Only admins can edit groups", Toast.LENGTH_LONG).show();
-                }
-            }
+        editGroup.setOnClickListener((v) -> {
+            Log.d(TAG, "Edit group clicked");
+            if (isAdmin) {
+                EditGroupFragment editGroupFragment = EditGroupFragment.newInstance(group);
+                FragmentManager manager = getActivity().getSupportFragmentManager();
+                manager.beginTransaction().replace(R.id.fragment_container, editGroupFragment).commit();
 
+            } else {
+                Toast.makeText(getContext(), "Only admins can edit groups", Toast.LENGTH_LONG).show();
+            }
         });
         //Goes to GroupTransactionHistory Activity
         groupHistory.setOnClickListener(new View.OnClickListener() {
